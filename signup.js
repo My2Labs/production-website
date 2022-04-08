c8e5f890cedce20ba25077900512ceed03ac49bb
Sharron <sking171@ford.com>
2022-04-08 08:05:13 -0500

    Edit js source to try to get GitHub to access js.

--- signup.js
+++ signup.js
@@ -1,20 +1,19 @@
 const form = document.querySelectorAll("form")
 const submitbutton = document.querySelector(".submitbutton")
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault()
 
-    const formData = new FormData(event.target)
-    const firstName = formData.get(".firstname")
-    const lastName = formData.get(".lastname")
-    const email = formData.get(".emailaddress")
+//     const formData = new FormData(event.target)
+//     const firstName = formData.get(".firstname")
+//     const lastName = formData.get(".lastname")
+//     const email = formData.get(".emailaddress")
 
-    console.log(firstName, lastName, email)
-    event.target.reset()
-})
+//     console.log(firstName, lastName, email)
+//     event.target.reset()
+// })
 
 submitbutton.addEventListener(`click`, (e) => {
 
     console.log(values);
 });
-W
