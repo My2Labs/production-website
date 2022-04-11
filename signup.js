ad2856dffdda61761e7170f5a65930629a32c41d
Sharron <sking171@ford.com>
2022-04-11 10:15:59 -0500

    npx standard

--- signup.js
+++ signup.js
@@ -1,29 +1,28 @@
-const button = document.querySelector("button");
-const submitButton = document.querySelector("#submitbutton")
-const subscription = document.querySelector("#subscription")
+const button = document.querySelector('button')
+const submitButton = document.querySelector('#submitbutton')
+const subscription = document.querySelector('#subscription')
 
+subscription.addEventListener('submit', (event) => {
+  event.preventDefault()
 
-subscription.addEventListener("submit", (event) => {
-    event.preventDefault();
+  const formData = new FormData(event.target)
+  const firstName = formData.get('first-name')
+  const lastName = formData.get('last-name')
+  const email = formData.get('email')
+  const frequency = formData.get('frequency')
 
-    const formData = new FormData(event.target);
-    const firstName = formData.get("first-name");
-    const lastName = formData.get("last-name");
-    const email = formData.get("email");
-    const frequency = formData.get("frequency");
+  localStorage.setItem('first-name', firstName)
+  localStorage.setItem('last-name', lastName)
+  localStorage.setItem('email', email)
+  localStorage.setItem('frequency', email)
 
-    localStorage.setItem("first-name", firstName);
-    localStorage.setItem("last-name", lastName);
-    localStorage.setItem("email", email);
-    localStorage.setItem("frequency", email);
-
-    console.log(firstName, lastName, email, frequency);
+  console.log(firstName, lastName, email, frequency)
 })
 
 console.log(
-    "Returned local storage:",
-    localStorage.getItem("first-name"),
-    localStorage.getItem("last-name"),
-    localStorage.getItem("last-name"),
-    localStorage.getItem("last-name")
-);
+  'Returned local storage:',
+  localStorage.getItem('first-name'),
+  localStorage.getItem('last-name'),
+  localStorage.getItem('last-name'),
+  localStorage.getItem('last-name')
+)
