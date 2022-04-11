e1c0d773ba7e340705851b9be74e6328a4842598
Sharron <sking171@ford.com>
2022-04-11 10:45:29 -0500

    Edit responsiveness.

--- index.js
+++ index.js
@@ -5,77 +5,77 @@ const newsApiUrl = 'https://sharron-news-api-proxy.herokuapp.com/'
 const spinner = document.querySelector('.spinner')
 
 fetch(newsApiUrl)
-  .then(response => response.json())
-  .then(response => {
-    const articleheader1 = document.querySelector('.articleheader1')
-    const article1 = document.querySelector('.article1')
-    const article = response.articles[1]
-    articleheader1.append(article.title)
-    article1.append(article.description)
-    return article
-  }).then(response => {
-    const articleimg1 = document.querySelector('.articleimg1')
-    const img = document.createElement('img')
-    img.src = `${response.urlToImage}`
-    articleimg1.append(img)
-    const articlelink1 = document.querySelector('.articlelink1')
-    articlelink1.href = response.url
-    spinner.classList.add('hidden')
-    main.append(response)
-  })
+    .then(response => response.json())
+    .then(response => {
+        const articleheader1 = document.querySelector('.articleheader1')
+        const article1 = document.querySelector('.article1')
+        const article = response.articles[1]
+        articleheader1.append(article.title)
+        article1.append(article.description)
+        return article
+    }).then(response => {
+        const articleimg1 = document.querySelector('.articleimg1')
+        const img = document.createElement('img')
+        img.src = `${response.urlToImage}`
+        articleimg1.append(img)
+        const articlelink1 = document.querySelector('.articlelink1')
+        articlelink1.href = response.url
+        spinner.classList.add('hidden')
+        main.append(response)
+    })
 
 fetch(newsApiUrl)
-  .then(response => response.json())
-  .then(response => {
-    const articleheader2 = document.querySelector('.articleheader2')
-    const article2 = document.querySelector('.article2')
-    const article = response.articles[4]
-    articleheader2.append(article.title)
-    article2.append(article.description)
-    return article
-  }).then(response => {
-    const articleimg2 = document.querySelector('.articleimg2')
-    const img = document.createElement('img')
-    img.src = `${response.urlToImage}`
-    articleimg2.append(img)
-    const articlelink2 = document.querySelector('.articlelink2')
-    articlelink2.href = response.url
-  })
+    .then(response => response.json())
+    .then(response => {
+        const articleheader2 = document.querySelector('.articleheader2')
+        const article2 = document.querySelector('.article2')
+        const article = response.articles[4]
+        articleheader2.append(article.title)
+        article2.append(article.description)
+        return article
+    }).then(response => {
+        const articleimg2 = document.querySelector('.articleimg2')
+        const img = document.createElement('img')
+        img.src = `${response.urlToImage}`
+        articleimg2.append(img)
+        const articlelink2 = document.querySelector('.articlelink2')
+        articlelink2.href = response.url
+    })
 
 fetch(newsApiUrl)
-  .then(response => response.json())
-  .then(response => {
-    console.log(response)
-    const articleheader3 = document.querySelector('.articleheader3')
-    const article3 = document.querySelector('.article3')
-    const article = response.articles[3]
-    articleheader3.append(article.title)
-    article3.append(article.description)
-    return article
-  }).then(response => {
-    const articleimg3 = document.querySelector('.articleimg3')
-    const img = document.createElement('img')
-    img.src = `${response.urlToImage}`
-    articleimg3.append(img)
-    const articlelink3 = document.querySelector('.articlelink3')
-    articlelink3.href = response.url
-  })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        const articleheader3 = document.querySelector('.articleheader3')
+        const article3 = document.querySelector('.article3')
+        const article = response.articles[3]
+        articleheader3.append(article.title)
+        article3.append(article.description)
+        return article
+    }).then(response => {
+        const articleimg3 = document.querySelector('.articleimg3')
+        const img = document.createElement('img')
+        img.src = `${response.urlToImage}`
+        articleimg3.append(img)
+        const articlelink3 = document.querySelector('.articlelink3')
+        articlelink3.href = response.url
+    })
 
 fetch(newsApiUrl)
-  .then(response => response.json())
-  .then(response => {
-    console.log(response)
-    const articleheader4 = document.querySelector('.articleheader4')
-    const article4 = document.querySelector('.article4')
-    const article = response.articles[7]
-    articleheader4.append(article.title)
-    article4.append(article.description)
-    return article
-  }).then(response => {
-    const articleimg4 = document.querySelector('.articleimg4')
-    const img = document.createElement('img')
-    img.src = `${response.urlToImage}`
-    articleimg4.append(img)
-    const articlelink4 = document.querySelector('.articlelink4')
-    articlelink4.href = response.url
-  })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        const articleheader4 = document.querySelector('.articleheader4')
+        const article4 = document.querySelector('.article4')
+        const article = response.articles[7]
+        articleheader4.append(article.title)
+        article4.append(article.description)
+        return article
+    }).then(response => {
+        const articleimg4 = document.querySelector('.articleimg4')
+        const img = document.createElement('img')
+        img.src = `${response.urlToImage}`
+        articleimg4.append(img)
+        const articlelink4 = document.querySelector('.articlelink4')
+        articlelink4.href = response.url
+    })
